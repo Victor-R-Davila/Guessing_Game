cc0677d498064f6d6ef0474ad524f5b9569ccd56
Victor-R-Davila <vicdtx@gmail.com>
2020-06-01 10:41:34 -0400

    Added the ability to check if the answer is a non-zero integer.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - The program should contain one function, one loop, and one if statement.
 - It should also be more than 20 lines of code but less than 50 lines of code.
 - The number of line of code in this program are:
-26
+42
 >
-Date: 2020-05-31, Time: 21:49:57
+Date: 2020-06-01, Time: 10:40:14
 
--- guessinggame.sh
+++ guessinggame.sh
@@ -3,15 +3,28 @@
 # This is a game in which you guess the number of files in a directory.
 
 echo "We are going to play a guessing game."
+files=$(pwd | ls | wc -l) 
 
+#Function to obtain the guess from the player
 function gettheguess {
-echo "Guess the number of files there are in the current directory: "
-read guess
+	echo "Guess the number of files there are in the current directory: "
+	read guess
 }
 
-gettheguess
+#function to check if the guess from the player is a number or not.
+function numberornot {
+	if ! [[ "guess" =~ ^[0-9]+$ ]]
+   		then
+    		echo "Sorry non-negative integers only"
+        	gettheguess
+     	else
+     		checktheguess
+	fi
+}
 
-files=$(pwd | ls | wc -l) 
+#function to check the guess and compare it to the correct number of files.
+function checktheguess {
+
 while [[ $guess -ne $files ]]; do
 	if [[ $guess -gt $files ]]
 	then
@@ -23,5 +36,8 @@ while [[ $guess -ne $files ]]; do
 		gettheguess
 	fi
 done
-
+}
+gettheguess
+numberornot
+checktheguess
 echo "You got it!! $guess is the number of files in the directory."
